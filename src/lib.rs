bfc2a0facfac204e604ee51e33babf3bc97ce06a
Sergio Arroutbi <sarroutb@redhat.com>
2025-02-20 15:16:59 +0100

    Add serialization/deserialization for the request (#2)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,308 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/*
+    This library allows serialization/deserialization of structures having to do with
+    Keylime push model. In particular, it provides methods to serialize/deserialize
+    structures required to implement next OpenAPI spec:
+    openapi: 3.0.0
+info:
+  title: Capabilities Negotiation API
+  version: 1.0.0
+components:
+  schemas:
+    AttestationRequest: # Request schema
+      type: object
+      properties:
+        data:
+          type: object
+          properties:
+            type:
+              type: string
+              example: attestation
+            attributes:
+              type: object
+              properties:
+                evidence_supported:
+                  type: array
+                  items:
+                    type: object
+                    properties:
+                      evidence_class:
+                        type: string
+                        example: certification
+                      evidence_type:
+                        type: string
+                        example: tpm_quote
+                      agent_capabilities:
+                        type: object
+                        properties:
+                          spec_version:
+                            type: string
+                            example: "2.0"
+                          hash_algorithms:
+                            type: array
+                            items:
+                              type: string
+                              example: sha3_512
+                          signing_schemes:
+                            type: array
+                            items:
+                              type: string
+                              example: rsassa
+                          attestation_keys:
+                            type: array
+                            items:
+                              type: object
+                              properties:
+                                key_class:
+                                  type: string
+                                  example: private_key
+                                key_identifier:
+                                  type: string
+                                  example: "..."
+                                key_algorithm:
+                                  type: string
+                                  example: rsa
+                                public_hash:
+                                  type: string
+                                  example: "..."
+                        required:
+                          - spec_version
+                          - hash_algorithms
+                          - signing_schemes
+                          - attestation_keys
+                      version:  # Only present for some evidence types
+                        type: string
+                        example: "2.1"
+                boot_time:
+                  type: string
+                  format: date-time
+                  example: "2024-11-12T16:21:17Z"
+              required:
+                - evidence_supported
+                - boot_time
+          required:
+            - type
+            - attributes
+      required:
+        - data
+    AttestationResponse: # Response schema
+      type: object
+      properties:
+        data:
+          type: object
+          properties:
+            type:
+              type: string
+              example: attestation
+            attributes:
+              type: object
+              properties:
+                evidence_collected: # Nuevo campo para la respuesta
+                  type: array
+                  items:
+                    type: object
+                    properties:
+                      evidence_class:
+                        type: string
+                        example: certification
+                      evidence_type:
+                        type: string
+                        example: tpm_quote
+                      chosen_parameters:
+                        type: object
+                        properties:
+                          nonce:
+                            type: string
+                            example: "..."
+                          pcr_selection:
+                            type: array
+                            items:
+                              type: integer
+                              example: 0
+                          hash_algorithm:
+                            type: string
+                            example: sha384
+                          signing_scheme:
+                            type: string
+                            example: rsassa
+                          attestation_key:
+                            type: object
+                            properties:
+                              key_class:
+                                type: string
+                                example: private_key
+                              key_identifier:
+                                type: string
+                                example: "..."
+                              key_algorithm:
+                                type: string
+                                example: rsa
+                              public_hash:
+                                type: string
+                                example: "..."
+                          starting_offset: # Only for some evidence types
+                            type: integer
+                            example: 25
+                boot_time:
+                  type: string
+                  format: date-time
+                  example: "2024-11-12T16:21:17Z"
+              required:
+                - evidence_collected
+                - boot_time
+          required:
+            - type
+            - attributes
+      required:
+        - data
+paths:
+  /agents/{id}/attestations:
+    post:
+      summary: Create Attestation Data for Agent
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: string
+          required: true
+          description: ID of the agent
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: '#/components/schemas/AttestationRequest' # Usa AttestationRequest
+      responses:
+        '201':
+          description: Attestation data created successfully
+        '200':
+          description: Attestation data retrieved successfully
+          content:
+            application/json:
+              schema:
+                $ref: '#/components/schemas/AttestationResponse' # Usa AttestationResponse
+        '400':
+          description: Bad request (e.g., invalid input)
+        # other possible response codes
+*/
+
+use serde::{Deserialize, Serialize};
+
+// Define the structure for the AttestationRequest:
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AttestationRequest {
+    pub data: Data,
 }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    pub type_: String,
+    pub attributes: Attributes,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Attributes {
+    pub evidence_supported: Vec<EvidenceSupported>,
+    pub boot_time: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EvidenceSupported {
+    pub evidence_class: String,
+    pub evidence_type: String,
+    pub agent_capabilities: AgentCapabilities,
+    pub version: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AgentCapabilities {
+    pub spec_version: String,
+    pub hash_algorithms: Vec<String>,
+    pub signing_schemes: Vec<String>,
+    pub attestation_keys: Vec<AttestationKeys>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AttestationKeys {
+    pub key_class: String,
+    pub key_identifier: String,
+    pub key_algorithm: String,
+    pub public_hash: String,
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn serialize_request() {
+        // Create a new AttestationRequest object and serialize it to JSON
+        let request = AttestationRequest {
+            data: Data {
+                type_: "attestation".to_string(),
+                attributes: Attributes {
+                    evidence_supported: vec![
+                        EvidenceSupported {
+                            evidence_class: "certification".to_string(),
+                            evidence_type: "tpm_quote".to_string(),
+                            agent_capabilities: AgentCapabilities {
+                                spec_version: "2.0".to_string(),
+                                hash_algorithms: vec!["sha3_512".to_string()],
+                                signing_schemes: vec!["rsassa".to_string()],
+                                attestation_keys: vec![
+                                    AttestationKeys {
+                                        key_class: "private_key".to_string(),
+                                        key_identifier: "att_key_identifier".to_string(),
+                                        key_algorithm: "rsa".to_string(),
+                                        public_hash: "cd293be6cea034bd45a0352775a219ef5dc7825ce55d1f7dae9762d80ce64411".to_string(),
+                                    },
+                                ],
+                            },
+                            version: Some("2.1".to_string()),
+                        },
+                    ],
+                    boot_time: "2024-11-12T16:21:17Z".to_string(),
+                },
+            },
+        };
+        let json = serde_json::to_string(&request).unwrap();
+        println!("{}", json);
+        assert_eq!(json, r#"{"data":{"type":"attestation","attributes":{"evidence_supported":[{"evidence_class":"certification","evidence_type":"tpm_quote","agent_capabilities":{"spec_version":"2.0","hash_algorithms":["sha3_512"],"signing_schemes":["rsassa"],"attestation_keys":[{"key_class":"private_key","key_identifier":"att_key_identifier","key_algorithm":"rsa","public_hash":"cd293be6cea034bd45a0352775a219ef5dc7825ce55d1f7dae9762d80ce64411"}]},"version":"2.1"}],"boot_time":"2024-11-12T16:21:17Z"}}}"#);
+    }
+
+    #[test]
+    fn deserialize_request() {
+        // Create a JSON string and deserialize it to an AttestationRequest object
+        let json = r#"
+        {
+            "data": {
+                "type":"attestation",
+                "attributes": {
+                    "evidence_supported":[{"evidence_class":"certification",
+                                            "evidence_type":"tpm_quote",
+                                            "agent_capabilities":{"spec_version":"2.0",
+                                            "hash_algorithms":["sha3_512"],
+                                            "signing_schemes":["rsassa"],
+                                            "attestation_keys":[{"key_class":"private_key","key_identifier":"att_key_identifier",
+                                                                "key_algorithm":"rsa",
+                                                                "public_hash":"cd293be6cea034bd45a0352775a219ef5dc7825ce55d1f7dae9762d80ce64411"}]},
+                                            "version":"2.1"}],
+                     "boot_time":"2024-11-12T16:21:17Z"
+                }
+            }
+        }"#;
+        let request: AttestationRequest = serde_json::from_str(json).unwrap();
+        assert_eq!(request.data.type_, "attestation");
+        assert_eq!(request.data.attributes.evidence_supported[0].evidence_class, "certification");
+        assert_eq!(request.data.attributes.evidence_supported[0].version, Some("2.1".to_string()));
+        assert_eq!(request.data.attributes.evidence_supported[0].evidence_type, "tpm_quote");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.spec_version, "2.0");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.hash_algorithms[0], "sha3_512");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.signing_schemes[0], "rsassa");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.attestation_keys[0].key_class, "private_key");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.attestation_keys[0].key_identifier, "att_key_identifier");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.attestation_keys[0].key_algorithm, "rsa");
+        assert_eq!(request.data.attributes.evidence_supported[0].agent_capabilities.attestation_keys[0].public_hash,
+            "cd293be6cea034bd45a0352775a219ef5dc7825ce55d1f7dae9762d80ce64411");
+        assert_eq!(request.data.attributes.boot_time, "2024-11-12T16:21:17Z");
     }
 }
